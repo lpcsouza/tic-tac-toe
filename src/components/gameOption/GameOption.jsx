d318c54284c7a7fec1a4e8a5e26505798f6c0ca0
Lpcsouza <17843059+lpcsouza@users.noreply.github.com>
2024-10-25 20:43:07 -0300

    criado estados no GameOption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,7 +1,15 @@
 import styles from "./GameOption.module.css"
+import Icon from "../icon/Icon"
 
-function GameOption() {
-  return <div className={styles.gameOption}></div>
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px" />
+
+function GameOption({ status }) {
+  return (
+    <div className={styles.gameOption}>
+      {status === 1 && <GameIcon iconName="circle" />}
+      {status === -1 && <GameIcon iconName="x" />}
+    </div>
+  )
 }
 
 export default GameOption
